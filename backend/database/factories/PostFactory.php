292b5f881a8368784e10ab5f67ce66316a03f839
nass22 <nacer.smr@gmail.com>
2022-02-09 13:28:17 +0100

    add img in table post

--- backend/database/factories/PostFactory.php
+++ backend/database/factories/PostFactory.php
@@ -18,7 +18,8 @@ class PostFactory extends Factory
             "title" => $this->faker->sentence(),
             "content" => $this->faker->paragraph(),
             "location" => $this->faker->state(),
-            "artists" => $this->faker->word()
+            "artists" => $this->faker->word(),
+            "image" => "https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTkyNjR8MHwxfHNlYXJjaHwxfHxmZXN0aXZhbHxlbnwwfHx8fDE2NDQ0MDE2MTc&ixlib=rb-1.2.1&q=80&w=1080"
         ];
     }
 }
